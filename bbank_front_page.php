de1633022951c946f2e3991aafde6985c7479dbf
Nora El-Zein <34493664+norael-zein@users.noreply.github.com>
2024-09-24 17:47:17 +0200

    changes

--- bbank_front_page.php
+++ bbank_front_page.php
@@ -5,8 +5,6 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>Blood Bank front page</title>
     <link href="BloodAlert_logo.png" rel="icon">
-    <link rel="stylesheet" href="stylesheet/styles.css" />
-    <link href="stylesheet/reset.css" rel="stylesheet">
     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;700;800&display=swap">
 </head>
 
