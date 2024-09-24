7d9cb41871e54c003630405323b38bad68748115
Mariam <mariam.alabi@outlook.com>
2024-09-24 16:41:10 +0200

    basic form to reg page

---
+++ create_account.php
@@ -0,0 +1,22 @@
+<h1>Donor Register Page</h1>
+<form>
+    <label for="fname"> First Name</label>
+    <input type="text" id="fname"> <br>
+
+    <label for="lname"> Last Name</label>
+    <input type="text" id="lname"> <br>
+
+    <label for="address"> Address</label>
+    <select id = "address">
+        <!-- Change this later so that the regions comes from the database -->
+        <option value = "Stockholm">Stockholm</option>
+        <option value = "Jönköping">Jönköping</option>
+        <option value = "Kalmar">Kalmar</option>
+    </select> <br>
+
+    <label for="age"> Age</label>
+    <input type="number" id="age"> <br>
+
+
+
+</form>
