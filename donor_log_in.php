d7240b2c4b9f6bd9399cb6e482a9fd223d8b2cbc
Nora El-Zein <34493664+norael-zein@users.noreply.github.com>
2024-09-24 18:52:12 +0200

    changed paths

--- donor_log_in.php
+++ donor_log_in.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>Blood Donor Log in</title>
-    <link rel="stylesheet" href="styles.css" />
+    <link rel="stylesheet" href="stylesheet/styles.css" />
     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;700;800&display=swap">
 </head>
 
