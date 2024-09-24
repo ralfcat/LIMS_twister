aa0372ff94e88e6211208ed8e67dabb20f4ce8e5
Nora El-Zein <34493664+norael-zein@users.noreply.github.com>
2024-09-24 18:14:20 +0200

    added stylesheets

--- donor_front_page.php
+++ donor_front_page.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>Donor front page</title>
+    <link rel="stylesheet" href="stylesheet/styles.css" />
     <link href="BloodAlert_logo.png" rel="icon">
     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;700;800&display=swap">
 </head>
