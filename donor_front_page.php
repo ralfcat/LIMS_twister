d0aaff9032cf973ba8374a5e5e3a644533b90d98
Nora El-Zein <34493664+norael-zein@users.noreply.github.com>
2024-09-24 16:58:11 +0200

    added information

---
+++ donor_front_page.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Donor front page</title>
+    <link rel="stylesheet" href="styles.css" />
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;700;800&display=swap">
+</head>
+
+<body>
+
+</body>
