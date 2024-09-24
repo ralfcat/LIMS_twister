27e9946fd1b1f3f165320dfb8220a55fed07181f
Nora El-Zein <34493664+norael-zein@users.noreply.github.com>
2024-09-24 17:17:53 +0200

    added navigation

--- bbank_front_page.php
+++ bbank_front_page.php
@@ -4,10 +4,17 @@
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>Blood Bank front page</title>
+    <link href="BloodAlert_logo.png" rel="icon">
     <link rel="stylesheet" href="styles.css" />
     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;700;800&display=swap">
 </head>
 
 <body>
+<nav> <!--navigation-->
+            <ul>
+                <li class="active"><a href="bbank_front_page.php">Blood level inventory</a></li>
+                <li><a href="">Profile</a></li>
+            </ul>
+    </nav>
 
 </body>
--- donor_front_page.php
+++ donor_front_page.php
@@ -4,10 +4,19 @@
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>Donor front page</title>
+    <link href="BloodAlert_logo.png" rel="icon">
     <link rel="stylesheet" href="styles.css" />
     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;700;800&display=swap">
 </head>
 
 <body>
+
+    <nav> <!--navigation-->
+            <ul>
+                <li class="active"><a href="donor_front_page.php">My donations</a></li>
+                <li><a href="">Profile</a></li>
+            </ul>
+
+    </nav>
 
 </body>
