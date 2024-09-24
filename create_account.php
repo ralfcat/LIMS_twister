7da3966c49508a91821cb5b279fd5c1fee013fdd
Mariam <mariam.alabi@outlook.com>
2024-09-24 17:48:35 +0200

    finished form

--- create_account.php
+++ create_account.php
@@ -1,85 +1,104 @@
 <body>
 
-<h1>Donor Register Page</h1>
+    <h1>Donor Register Page</h1>
 
-<form>
-    <label for="fname"> First Name</label>
-    <input type="text" id="fname"> <br>
+    <form>
+        <label for="fname"> First Name</label>
+        <input type="text" id="fname"> <br>
 
-    <label for="lname"> Last Name</label>
-    <input type="text" id="lname"> <br>
+        <label for="lname"> Last Name</label>
+        <input type="text" id="lname"> <br>
 
-    <label for="address"> Address</label>
-    <select id="address">
-        <!-- Change this so that the regions comes from the database -->
-        <option value="Stockholm">Stockholm</option>
-        <option value="Jönköping">Jönköping</option>
-        <option value="Kalmar">Kalmar</option>
-    </select> <br>
+        <label for="address"> Address</label>
+        <select id="address">
+            <!-- Change this so that the regions comes from the database -->
+            <option value="Stockholm">Stockholm</option>
+            <option value="Jönköping">Jönköping</option>
+            <option value="Kalmar">Kalmar</option>
+        </select> <br>
 
-    <label for="age"> Age</label>
-    <input type="number" id="age"> <br>
+        <label for="age"> Age</label>
+        <input type="number" id="age"> <br>
 
-    <label for="email"> Email</label>
-    <input type="text" id="email"> <br>
+        <label for="sex"> Sex</label>
+        <select id="sexes" onchange="check_preg()">
+            <option value="Male">Male</option>
+            <option value="Female">Female</option>
+            
+        </select> 
 
-    <label for="password"> Password</label>
-    <input type="password" id="password"> <br>
+        <label for="preggo" id="pr"></label>
+        <select hidden id="preg">
+            <option value="No">No</option>
+            <option value="Yes">Yes</option>
+        </select> <br>
 
-    <label for="re-password"> Re-type Password</label>
-    <input type="password" id="re-password"> <br>
+        <label for="email"> Email</label>
+        <input type="text" id="email"> <br>
 
-    <label for="btype"> Blood Type</label>
-    <select id="btype">
-        <option value="o-">O-</option>
-        <option value="o+">O+</option>
-        <option value="b-">B-</option>
-        <option value="b+">B+</option>
-        <option value="a-">A-</option>
-        <option value="a+">A+</option>
-        <option value="ab-">AB-</option>
-        <option value="ab+">AB+</option>
-        <option value="idk">Unsure</option>
-    </select> <br>
+        <label for="password"> Password</label>
+        <input type="password" id="password"> <br>
 
-    <label for="donated"> Have you donated blood before?</label>
-    <select id="donateds" onchange = "last_donated_date()">
-        <option value="No">No</option>
-        <option value="Yes">Yes</option>
-    </select> <br>
+        <label for="re-password"> Re-type Password</label>
+        <input type="password" id="re-password"> <br>
 
-    <label for="donate-date" id = "dn"></label>
-    <input type="hidden" id="donate-date"> <br>
+        <label for="btype"> Blood Type</label>
+        <select id="btype">
+            <option value="o-">O-</option>
+            <option value="o+">O+</option>
+            <option value="b-">B-</option>
+            <option value="b+">B+</option>
+            <option value="a-">A-</option>
+            <option value="a+">A+</option>
+            <option value="ab-">AB-</option>
+            <option value="ab+">AB+</option>
+            <option value="idk">Unsure</option>
+        </select> <br>
 
-    <input type="submit" value="Register">
-    <p id="demo"></p>
+        <label for="donated"> Have you donated blood before?</label>
+        <select id="donateds" onchange="last_donated_date()">
+            <option value="No">No</option>
+            <option value="Yes">Yes</option>
+        </select> 
+
+        <label for="donate-date" id="dn"></label>
+        <input type="hidden" id="donate-date"> <br>
+
+        <input type="submit" value="Register">
+        <p id="demo"></p>
 
 
 
 
 
-</form>
+    </form>
 
 </body>
 
 <script>
-
-    console.log('Hello');
-
-
-
-    function last_donated_date() {
-        var donated = document.getElementById("donateds").value;
-        if (donated === "No") {
-            document.getElementById("dn").innerHTML = ''
-            document.getElementById("donate-date").type = 'hidden'
-        } else {
-            document.getElementById("dn").innerHTML = 'When did you last donate?'
-            document.getElementById("donate-date").type = 'date'
-        }
-
-        
-
+function last_donated_date() {
+    var donated = document.getElementById("donateds").value;
+    if (donated === "No") {
+        document.getElementById("dn").innerHTML = ''
+        document.getElementById("donate-date").type = 'hidden'
+    } else {
+        document.getElementById("dn").innerHTML = 'When did you last donate?'
+        document.getElementById("donate-date").type = 'date'
     }
-    // document.getElementById("donateds").addEventListener("change", last_donated_date);
+}
+
+function check_preg() {
+    let sel = document.getElementById('preg');
+    let label = document.getElementById('pr');
+    let hidden = sel.getAttribute("hidden");
+    var sex = document.getElementById("sexes").value;
+    if (sex === "Female") {
+        sel.removeAttribute("hidden");
+        label.innerHTML = 'Are you pregnant or breastfeeding?';
+
+    } else {
+        sel.setAttribute("hidden", "hidden");
+        label.innerHTML = '';
+    }
+}
 </script>
