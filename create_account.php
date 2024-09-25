9c97c7ea8540cb14bbee86e7edf0289a56eb09bf
Mariam <mariam.alabi@outlook.com>
2024-09-25 17:16:28 +0200

    email check

--- create_account.php
+++ create_account.php
@@ -1,16 +1,31 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "root";
+$dbname = "twister";
+
+// Create connection
+$link = mysqli_connect($servername, $username, $password, $dbname);
+
+// Check if connection is established
+if (mysqli_connect_error()) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+?>
+
 <body>
 
     <h1>Donor Register Page</h1>
 
-    <form>
+    <form action=<?php echo $_SERVER['PHP_SELF']; ?> onsubmit="return validate_form()" method="POST">
         <label for="fname"> First Name</label>
-        <input type="text" id="fname"> <br>
+        <input type="text" id="fname" name = "fname"> <br>
 
         <label for="lname"> Last Name</label>
-        <input type="text" id="lname"> <br>
+        <input type="text" id="lname" name = "lname"> <br>
 
         <label for="address"> Address</label>
-        <select id="address">
+        <select id="address" name = "address">
             <!-- Change this so that the regions comes from the database -->
             <option value="Stockholm">Stockholm</option>
             <option value="Jönköping">Jönköping</option>
@@ -18,32 +33,32 @@
         </select> <br>
 
         <label for="age"> Age</label>
-        <input type="number" id="age"> <br>
+        <input type="number" id="age" name = "age"> <br>
 
         <label for="sex"> Sex</label>
-        <select id="sexes" onchange="check_preg()">
+        <select id="sexes" onchange="check_preg()" name = "sexes">
             <option value="Male">Male</option>
             <option value="Female">Female</option>
-            
-        </select> 
+
+        </select>
 
         <label for="preggo" id="pr"></label>
-        <select hidden id="preg">
+        <select hidden id="preg" name = "preg" value = "No">
             <option value="No">No</option>
             <option value="Yes">Yes</option>
         </select> <br>
 
         <label for="email"> Email</label>
-        <input type="text" id="email"> <br>
+        <input type="text" id="email" name = "email"> <br>
 
         <label for="password"> Password</label>
-        <input type="password" id="password"> <br>
+        <input type="password" id="password" name = "password"> <br>
 
         <label for="re-password"> Re-type Password</label>
-        <input type="password" id="re-password"> <br>
+        <input type="password" id="re-password" name = "repassword"> <br>
 
         <label for="btype"> Blood Type</label>
-        <select id="btype">
+        <select id="btype" name = "btype">
             <option value="o-">O-</option>
             <option value="o+">O+</option>
             <option value="b-">B-</option>
@@ -56,13 +71,13 @@
         </select> <br>
 
         <label for="donated"> Have you donated blood before?</label>
-        <select id="donateds" onchange="last_donated_date()">
+        <select id="donateds" onchange="last_donated_date()" name = "donateds">
             <option value="No">No</option>
             <option value="Yes">Yes</option>
-        </select> 
+        </select>
 
         <label for="donate-date" id="dn"></label>
-        <input type="hidden" id="donate-date"> <br>
+        <input type="hidden" id="donate-date" name = "donateddate"> <br>
 
         <input type="submit" value="Register">
         <p id="demo"></p>
@@ -72,10 +87,76 @@
 
 
     </form>
+    <p id="error_msg"></p>
 
 </body>
 
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // $myear = $_POST['myear'];
+    // use email catherine_miller_356@outlook.com
+    echo '<script type="text/javascript">
+    let err = document.getElementById("error_msg");
+    document.getElementById("error_msg").innerHTML = "";
+    </script>';
+
+    $fname = $_POST["fname"];
+    $lname = $_POST["lname"];
+    $address = $_POST["address"];
+    $age = $_POST["age"];
+
+    $sexes = $_POST["sexes"];
+    $preg = $_POST["preg"];
+    $email = $_POST["email"];
+    $passowrd = $_POST["password"];
+    echo 'the email entered is ' . $email;
+
+    $repassword = $_POST["repassword"];
+    $btype = $_POST["btype"];
+    $donateds = $_POST["donateds"];
+    $donateddate = $_POST["donateddate"];
+
+
+    $sql_req = "SELECT COUNT(email) FROM Donor WHERE email = '" . $email. "'";
+    $res = $link->query($sql_req);
+    $row = $res->fetch_assoc();
+    echo 'the value of row is  ' . $row['COUNT(email)'] ;
+
+    if ($row > 0) {
+        // echo '<p id=error_msg>The email you entered already exists</p>';
+        echo '<script type="text/javascript">
+        let err = document.getElementById("error_msg");
+        document.getElementById("error_msg").innerHTML = "Email already exists";
+        </script>';
+    } 
+
+
+
+
+}
+
+
+?>
+
 <script>
+function validate_form() {
+    let pass = document.getElementById("password").value;
+    let repass = document.getElementById("re-password").value;
+
+    if (pass !==repass) {
+        document.getElementById("error_msg").innerHTML = "Passwords do not match";
+        return false;
+    }
+
+    // <label for="password"> Password</label>
+    //     <input type="password" id="password" name = "password"> <br>
+
+    //     <label for="re-password"> Re-type Password</label>
+    //     <input type="password" id="re-password" name = "repassword"> <br>
+
+
+}
+
 function last_donated_date() {
     var donated = document.getElementById("donateds").value;
     if (donated === "No") {
