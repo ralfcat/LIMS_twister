11d9d232c1112a0350988033097cbd4b5e52d2bf
emblapersson <115622805+emblapersson@users.noreply.github.com>
2024-09-27 12:02:11 +0200

    the blood bank info page is mainly done

--- bbank_info.php
+++ bbank_info.php
@@ -1,12 +1,50 @@
-<form action="Insertdata_donor.php" method="POST">
-    <h2> #Blood bank ID </h2>
-    Name:<input type="text" name="bbname"><br>
-    Email:<input type="text" name="bbemail"><br> 
-    Password:<input type="text" name="bbemail"><br> 
-    Region:<select id="bbregionid" name="bbregionid"> <!--Beginning of the drop-down menu-->
-    <option value="" disable selected></option> 
-    </select><br>
-    <input type="Save changes" value="Add">
-</form>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" type="image/png" href="BloodAlert_logo.png">
+    <title>Donor Dashboard</title>
+    <link rel="stylesheet" href="stylesheet/styles.css">
+</head>
+<body>
+    <header>
+        <div class="logo-container">
+            <img class="logo" src="BloodAlert_logo.png" alt="Logo">
+        </div>
+        <nav>
+            <ul>
+                <li><a href="bbank_front_page.php">Blood level inventory</a></li>
+                <li><a href="bbank_info.php">Profile</a></li>
+            </ul>
+        </nav>
+        <button class="logout-button">Log Out</button>
+    </header>
 
-<?php
+    <main>
+    <h1>Blood bank ID</h1>
+    <section class="donation-form">
+        <div class="form-container">
+            <!-- Left Column -->
+            <div class="form-column">
+                <h3>Name:</h3>
+                <input type="text" placeholder="Enter name">
+                <h3>Email:</h3>
+                <input type="text" placeholder="Enter email">
+                <h3>Password:</h3>
+                <input type="text" placeholder="Enter password">
+                <h3>Repeat password:</h3>
+                <input type="text" placeholder="Enter password">
+            </div>
+
+            <!-- Right Column -->
+            <div class="form-column">
+                <h3>Region:</h3>
+                <input type="text" placeholder="Region">
+            </div>
+        </div>
+        <button class="profile-changes-button">Save changes</button>
+    </section>
+</main>
+</body>
+</html>
