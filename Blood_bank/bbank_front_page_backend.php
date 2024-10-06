047f3fd9f9cc1ae5b410382c4d1003094c6f55a8
Mariam <mariam.alabi@outlook.com>
2024-10-06 15:35:46 +0200

    added bb login validation

--- Blood_bank/bbank_front_page_backend.php
+++ Blood_bank/bbank_front_page_backend.php
@@ -1,65 +1,34 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="icon" type="image/png" href="../BloodAlert_logo.png">
-    <title>Blood Bank front page</title>
-    <link rel="stylesheet" href="../stylesheet/reset.css">
-    <link rel="stylesheet" href="../stylesheet/styles2.css" />
-</head>
+<?php 
 
-<body>
-<header>
-        <div class="logo-container">
-            <img class="logo" src="../../Logo-and-text.png" alt="Logo">
-        </div>
-        <nav>
-            <ul>
-                <li class="active"><a href="bbank_front_page.php">Inventory</a></li>
-                <li><a href="../../bbank_info.php">Profile</a></li>
-            </ul>
-        </nav>
-        <button class="logout-button">Log Out</button>
-    </header>
-    <main>
-        <h1>Blood level inventory</h1>
 
-        <div class="bbank-container"> <!-- New container -->
-            <div class="Current_levels">
-                <h2>Current Levels</h2> <!--Implement the graph based on inventory levels here-->
-                <canvas id="myCanvas" width="500" height="400"></canvas> <!--Beginning of graph, we need to implement backend here-->
-            </div>
+function debug_to_console($data) {
+    $output = $data;
+    if (is_array($output))
+        $output = implode(',', $output);
 
-            <form action="#" method="POST" class="form-bbank"> <!--We need to change this-->
-                <h2>Notification Thresholds</h2>
-                <div class="input-group">
-                    <label>O+<input type="text" name="mname"></label>
-                    <label>O- <input type="text" name="mname"></label>
-                </div>
-                <div class="input-group">
-                    <label>A+<input type="text" name="mname"></label>
-                    <label>A- <input type="text" name="mname"></label>
-                </div>
-                <div class="input-group">
-                    <label>B+<input type="text" name="mname"></label>
-                    <label>B- <input type="text" name="mname"></label>
-                </div>
-                <div class="input-group">
-                    <label>AB+<input type="text" name="mname"></label>
-                    <label> AB-<input type="text" name="mname"></label>
-                </div>
-                <input class="save-donation-button" type="submit" value="SAVE">
-            </form>
-        </div>
+    echo "<script>console.log('Debug Objects: " . $output . "' );</script>";
+}
+$servername = "localhost";
+$username = "root";
+$password = "root";
+$dbname = "twister";
 
-        <section class="donation-form-bbank">
-            <h3>Bloodtype</h3>
-            <input type="text" placeholder="Enter Blodtype"> <!--We should have a dropdown list here-->
-            <h3>Units</h3>
-            <input type="text" placeholder="Enter Units">
-            <button class="add-donation-button">Add Donation</button>
-        </section>
-    </main>
+$link = mysqli_connect($servername, $username, $password, $dbname);
 
-</body>
+// Check if connection is established
+if (mysqli_connect_error()) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+
+
+$sql_req = "SELECT movies.mid, movies.mname, movies.myear, movies.mgenre, movies.mrating, genres.mgenre FROM Blood_Stock WHERE Blood_Stock.blood_bank_id = " . $mid;
+$res = $link->query($sql_req);
+$row = $res->fetch_assoc();
+$genre_req = "SELECT gid, mgenre FROM genres";
+$genre_res = $link->query($genre_req);
+
+
+$mname = $_POST['O+'];
+
+echo "<h1>". $mname."</h1>";
+debug_to_console($mname);
