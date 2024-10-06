58d748cb39926412ad6c05edac9c0d3b7e678c3a
Mariam <mariam.alabi@outlook.com>
2024-10-06 15:40:12 +0200

    bb log in validation

--- Blood_bank/bbank_log_in.php
+++ Blood_bank/bbank_log_in.php
@@ -1,3 +1,30 @@
+<?php 
+session_start();
+
+
+$servername = 'localhost';
+$username = 'root';
+$password = 'root';
+$dbname = 'twister';
+
+function write_js($data) {
+    $output = $data;
+    if (is_array($output))
+        $output = implode(',', $output);
+
+    echo "<script>" . $data . "</script>";
+}
+
+$link = mysqli_connect($servername, $username, $password, $dbname);
+
+// Check if connection is established
+if (mysqli_connect_error()) {
+    die('Connection failed: ' . mysqli_connect_error());
+}
+?>
+
+
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -9,13 +36,13 @@
     <link rel="stylesheet" href="../../stylesheet/styles2.css">
     <script>
         function validateForm() {
-            let email = document.forms["loginform"]["email"].value;
-            let password = document.forms["loginform"]["password"].value;
+            let email = document.forms["login-form"]["email"].value;
+            let password = document.forms["login-form"]["password"].value;
             
             let emailPattern = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
             
             document.getElementById("error-message").innerHTML = "";
-            document.getElementById("error-message2").innerHTML = "";
+
             if (email == "") {
                 document.getElementById("error-message").innerHTML = "Email must be filled out";
                 return false;
@@ -24,7 +51,7 @@
                 return false;
             }
             if (password == "") {
-                document.getElementById("error-message2").innerHTML = "Password must be filled out";
+                document.getElementById("error-message").innerHTML = "Password must be filled out";
                 return false;
             }
             return true;
@@ -44,7 +71,7 @@
     <div class=login-container> 
         <div class=login-form>
             <h2>Blood Bank Log in</h2>
-            <form action="my_donations.php" method="GET">
+            <form action=<?php echo $_SERVER['PHP_SELF'];?> onsubmit="return validateForm();" method="POST" id = "login-form">
                 <div class="input-group">
                     <input type="text" id="email" name="email" placeholder="Email" />
                 </div>
@@ -54,10 +81,59 @@
                 <button class="login_button">Log in</button>
             </form>
             <div class="new-user">
-                <p>Want to register? <a href="/blood-bank-register.php">Contact us</a></p>
+                <p>Want to register? <a href="/blood-bank-register.php">Contact us</a></p> <br>
+                <p id="error-message"></p>
             </div>
         </div>
     </div>
 </body>
 </html>
 
+<?php
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    write_js("console.log('in the post request fxn right now');");
+
+    $email = $_POST["email"];
+    $password = $_POST["password"];
+    $password_hash = hash("md5", $password);
+
+    $email_req = "SELECT COUNT(email), email FROM Blood_Bank WHERE email = '" . $email . "'";
+    $res = $link->query($email_req);
+    $row = $res->fetch_assoc();
+    $count = $row['COUNT(email)'];
+
+    if ($count == 0) { 
+        write_js("console.log('i enetered a non existing email');");
+
+        $str = "document.getElementById('error-message').innerHTML = 'The email you entered does not exist.';";
+        write_js($str);
+
+
+    } else{
+        $email_db = $row['email'];
+
+        write_js("console.log('i enetered a existing email that is present in the databse where the email is " . $email_db."');");
+        $email_req = "SELECT password FROM Blood_Bank WHERE email = '" . $email_db . "'";
+        $res = $link->query($email_req);
+        $row = $res->fetch_assoc();
+        $password_db = $row['password'];
+
+        if ($password_db != $password_hash) {
+            write_js("console.log('i entered the wrong password');");
+            $err = "document.getElementById('error-message').innerHTML = 'You entered the wrong password.';";
+            write_js($err);
+        } else {
+            $_SESSION['email'] = $email_db;
+            header("Location: bbank_front_page.php");
+            exit();
+        }
+
+
+    }
+
+
+}
+
+
+?>
