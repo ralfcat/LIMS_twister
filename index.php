b5a47cbfa56f3730893a937a61eeee78ec27411d
emblapersson <115622805+emblapersson@users.noreply.github.com>
2024-10-16 12:53:22 +0200

    Update index.php

--- index.php
+++ index.php
@@ -71,7 +71,7 @@
 <footer>
   <p>&copy; 2024 Blood Alert</p>
   <nav>
-    <a href="#about_us.html">About Us</a> |
+    <a href="about_us.html">About Us</a> |
     <a href="integrity_policy.html">Integrity Policy</a> |
     <a href="mailto:bloodalert.twister@gmail.com">Contact Us</a>
   </nav>
